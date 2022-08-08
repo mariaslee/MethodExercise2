d3ab59685f16f6909d9a4aceda2c66b7b58696a7
marias_lee84@yahoo.com <marias_lee84@yahoo.com>
2022-08-08 11:41:53 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MethodExercise2
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(20, 25));
+            Console.WriteLine(Subtract(30, 15));
+            Console.WriteLine(Multiply (5,10));
+            Console.WriteLine(Divide (75, 5));
+        }
+
+        public static int Add(int x, int y)
+        {
+            return (x + y);
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            return (x - y);
+        }
+        public static int Multiply(int x,int y)
+        {
+            return (x * y);
+        }
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+
+        }
+        
+    }
+        
+
+        
+}
